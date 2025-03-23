7c907c166d138074142c2eb9e88ec300d97d171c
tutw <tutorialeswo@gmail.com>
2025-03-23 17:00:40 +0100

    Update buscar_logos.py

--- buscar_logos.py
+++ buscar_logos.py
@@ -1,30 +1,44 @@
 import requests
 import xml.etree.ElementTree as ET
-import os
 
-# Obtén la clave API de una variable de entorno
-API_KEY = os.getenv('THESPORTSDB_API_KEY')
-API_URL = 'https://www.thesportsdb.com/api/v1/json/{}/search_all_teams.php?l=English%20Premier%20League'.format(API_KEY)
+# URL del repositorio de logos
+REPO_API_URL = 'https://api.github.com/repos/tv-logo/tv-logos/contents/countries'
 
-def buscar_logos_deportivos():
-    response = requests.get(API_URL)
-    if response.status_code == 200:
-        resultados = response.json()
-        return resultados
-    else:
-        print(f"Error: {response.status_code} - {response.text}")
+def obtener_logos_desde_github():
+    response = requests.get(REPO_API_URL)
+    if response.status_code != 200:
+        print(f"Error al acceder al repositorio: {response.status_code} - {response.text}")
         return None
 
+    paises = response.json()
+    logos = []
+
+    for pais in paises:
+        if pais['type'] == 'dir':
+            response_pais = requests.get(pais['url'])
+            if response_pais.status_code == 200:
+                archivos = response_pais.json()
+                for archivo in archivos:
+                    if archivo['type'] == 'file' and archivo['name'].endswith('.png'):
+                        logos.append({
+                            'nombre': archivo['name'],
+                            'url_logo': archivo['download_url']
+                        })
+            else:
+                print(f"Error al acceder al directorio {pais['name']}: {response_pais.status_code} - {response_pais.text}")
+
+    return logos
+
 def generar_lista_de_logos():
-    resultados = buscar_logos_deportivos()
-    if resultados:
+    logos = obtener_logos_desde_github()
+    if logos:
         root = ET.Element("logos")
-        for equipo in resultados['teams']:
+        for logo in logos:
             canal = ET.SubElement(root, "canal")
             nombre = ET.SubElement(canal, "nombre")
-            nombre.text = equipo['strTeam']
+            nombre.text = logo['nombre']
             url_logo = ET.SubElement(canal, "url_logo")
-            url_logo.text = equipo['strTeamBadge']
+            url_logo.text = logo['url_logo']
 
         tree = ET.ElementTree(root)
         tree.write("logos.xml", encoding="utf-8", xml_declaration=True)
