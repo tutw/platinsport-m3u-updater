b410ce578cc37c2ba7b95df6d65aea203a323442
tutw <tutorialeswo@gmail.com>
2025-03-25 17:09:26 +0100

    Update script_reproductor_web.py

--- script_reproductor_web.py
+++ script_reproductor_web.py
@@ -241,8 +241,7 @@ for line in page_content.splitlines():
     match = event_pattern.search(line)
     if match:
         date, time, league, teams, channels = match.groups()
-        channel_list = channels.split(') (')
-        channel_list = [channel.replace('(', '').replace(')', '') for channel in channel_list]  # Limpiar paréntesis
+        channel_list = re.findall(r'\((.*?)\)', channels)  # Encuentra todos los canales en el formato (CHxx)
         for channel in channel_list:
             channel_number = re.search(r'\d+', channel).group()
             channel_name = channel_names.get(channel_number, f'Channel {channel_number}')
