2a05fff44eccb1050f864ca73048f375d021a506
tutw <tutorialeswo@gmail.com>
2025-04-02 04:58:33 +0200

    Update script_agenda_DEPORTE-LIBRE.FANS.py

--- script_agenda_DEPORTE-LIBRE.FANS.py
+++ script_agenda_DEPORTE-LIBRE.FANS.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup  # Importar BeautifulSoup
 
 # URL del sitio a scrapear
 url = "https://deporte-libre.fans/schedule/"
